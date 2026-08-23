d43a3783fac045d1e136cf1d9a3eb19cd5cc08a8
Sonaligup-creator <sonaligup2000@gmail.com>
2026-08-23 15:32:29 +0530

    Refactor decommission script to streamline resource deletion process

--- scripts/decommission-bootstrap.ps1
+++ scripts/decommission-bootstrap.ps1
@@ -18,20 +18,10 @@ if ($SubscriptionId) {
     az account set --subscription $SubscriptionId
 }
 
-Push-Location (Join-Path $PSScriptRoot '..\terraform\bootstrap')
-try {
-    terraform init -input=false
-    if (Test-Path 'terraform.tfstate') {
-        terraform destroy -auto-approve -input=false
-    }
-}
-finally {
-    Pop-Location
-}
-
-if (az group exists --name $ResourceGroupName | Select-String -Pattern 'true') {
-    az group delete --name $ResourceGroupName --yes --no-wait
-    Write-Host "Bootstrap resource group deletion started: $ResourceGroupName"
+$exists = az group exists --name $ResourceGroupName
+if ($exists -eq 'true') {
+    az group delete --name $ResourceGroupName --yes
+    Write-Host "Bootstrap resource group deleted: $ResourceGroupName"
 }
 else {
     Write-Host "Bootstrap resource group does not exist: $ResourceGroupName"
